3aa5b15a66c86e32cb71156a2f2e7c823d6fca2c
sirgeorge <gsmulbah2500@gmail.com>
2023-04-18 02:59:44 +0000

    remove read me

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const {createApiUser}=require('./Apiuser/create-api-user')
-const {createApiKey}=require('./Apiuser/create-apikey')
-const {colAccessToken} = require('./Collection/create-accessToken')
-const {requestToPay}=require('./Collection/request-to-pay')
-const {disAccessToken} = require('./Disbursement/create-accessToken')
-const {disburseTransfer} = require('./Disbursement/transfer')
-const {remittAccessToken}=require('./Remittance/create-accessToken')
-const {remittTransfer } = require('./Remittance/transfer')
+const { createApiUser } = require('./Apiuser/create-api-user');
+const { createApiKey } = require('./Apiuser/create-apikey');
+const { colAccessToken } = require('./Collection/create-accessToken');
+const { requestToPay } = require('./Collection/request-to-pay');
+const { disAccessToken } = require('./Disbursement/create-accessToken');
+const { disburseTransfer } = require('./Disbursement/transfer');
+const { remittAccessToken } = require('./Remittance/create-accessToken');
+const { remittTransfer } = require('./Remittance/transfer');
+const { v4: uuidv4 } = require("uuid");
+
+
+module.exports = {
+  createApiUser,
+  createApiKey,
+  colAccessToken,
+  requestToPay,
+  disAccessToken,
+  disburseTransfer,
+  remittAccessToken,
+  remittTransfer,
+  uuidv4 
+
+};
